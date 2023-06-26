5a389c2d3036e1ad51163b33946052f8e4a0e8c2
Scarletbobcat <tien.hoang314@yahoo.com>
2023-06-26 10:39:58 -0400

    messing with google maps api

---
+++ sidewalks.py
@@ -0,0 +1,31 @@
+from datetime import datetime
+import googlemaps
+from pprint import pprint
+import json
+from dotenv import load_dotenv, find_dotenv
+import os
+
+#finds .env file and loads it
+load_dotenv(find_dotenv())
+
+#saves google key from .env file
+gKey = os.environ.get("GOOGLE_KEY")
+
+#creates connection to googlemaps api
+gmaps = googlemaps.Client(key = f"{gKey}")
+
+#gets information from api as json
+geocode_result = gmaps.geocode('1819 5th Avenue, Youngstown, OH')
+
+#makes json into readable string
+results_json = json.dumps(geocode_result)
+
+#loads json
+json_geocode = json.loads(results_json)
+
+# This prints the return json into a readable format
+# pprint(json_geocode[0])
+
+# Prints the latitude, longitude, and address
+pprint(json_geocode[0]['geometry']['location'])
+pprint(json_geocode[0]['formatted_address'])
