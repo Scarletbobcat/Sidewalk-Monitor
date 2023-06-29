9900a3c4571f99d690a4daa4748bcc767b8d58a4
Scarletbobcat <tien.hoang314@yahoo.com>
2023-06-29 14:18:02 -0400

    comments

--- heatmap.py
+++ heatmap.py
@@ -1,6 +1,9 @@
 import leafmap.foliumap as leafmap
+
+# filepath is where the csv file for heatmap is
 filepath = "https://raw.githubusercontent.com/opengeos/leafmap/master/examples/data/us_cities.csv"
 
+# creating heatmap settings
 m = leafmap.Map(tiles='stamentoner')
 m.add_heatmap(
     filepath,
@@ -10,10 +13,13 @@ m.add_heatmap(
     name="Heat map",
     radius=20,
 )
+
+# creates heatmap
 m
 
 # # colorbar for our real map
 # colors = ['red', 'yellow', 'green']
 # m.add_colorbar(colors = colors, vmin = 0, vmax = 5)
 
+# saves heatmap to html in current directory
 m.to_html("heatmap.html")
